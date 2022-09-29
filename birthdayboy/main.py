b0a2d01afd6c4172b5b23e0469f3c5977770bcb6
ahmed91abbas <ahmed91abbas@hotmail.com>
2022-09-29 15:37:41 +0200

    first version

--- birthdayboy/main.py
+++ birthdayboy/main.py
@@ -1,32 +1,47 @@
 # https://open.kattis.com/problems/birthdayboy
 import sys
-from datetime import date
+import datetime
 
 
 class Solver:
     def __init__(self, dates):
-        self.dates = dates
+        self.dates = sorted(dates, key=lambda tup: (tup[1], tup[0]) )
+        print(self.dates)
 
     def format_result(self, result):
-        return result
+        return result.strftime('%m-%d')
 
     def solve(self):
-        d0, m0 = (19, 9)
-        d1, m1 = self.dates[0]
-        d2, m2 = self.dates[1]
-        d3, m3 = self.dates[2]
-        d = self.get_days_diff(d0, m0, d1, m1)
-        print(d)
-        d = self.get_days_diff(d0, m0, d2, m2)
-        print(d)
-        d = self.get_days_diff(d0, m0, d3, m3)
-        print(d)
+        d1, m1 = self.dates[len(self.dates) - 1]
+        d2, m2 = self.dates[0]
+        diff = self.get_days_diff(d1, m1, d2, m2)
+        longest = diff
+        dates_with_longest_diff = [(self.dates[len(self.dates) - 1], self.dates[0])]
+        print((self.dates[0], self.dates[len(self.dates) - 1]), diff)
+        for i in range(1, len(self.dates)):
+            d1, m1 = self.dates[i-1]
+            d2, m2 = self.dates[i]
+            diff = self.get_days_diff(d1, m1, d2, m2)
+            print((self.dates[i-1], self.dates[i]), diff)
+            if diff > longest:
+                dates_with_longest_diff = [(self.dates[i-1], self.dates[i])]
+            elif diff == longest:
+                dates_with_longest_diff.append((self.dates[i-1], self.dates[i]))
+        # TODO pick nearest 27th of October
+        d, m = dates_with_longest_diff[0][1]
+        return self.get_previous_date(d, m)
 
     def get_days_diff(self, d1, m1, d2, m2):
-        d1 = date(2022, m1, d1)
-        d2 = date(2022, m2, d2)
+        year1 = 2022
+        year2 = year1 if m1 <= m2 else year1 + 1
+        d1 = datetime.date(year1, m1, d1)
+        d2 = datetime.date(year2, m2, d2)
         delta = d2 - d1
         return abs(delta.days)
+
+    def get_previous_date(self, d, m):
+        current = datetime.date(2022, m, d)
+        return current - datetime.timedelta(days=1)
 
 
 def run():
