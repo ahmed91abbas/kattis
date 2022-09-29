226e125fe6f22f007d5c831589d45a952778d9ec
ahmed91abbas <ahmed91abbas@hotmail.com>
2022-09-29 16:00:00 +0200

    pass test cases

--- birthdayboy/main.py
+++ birthdayboy/main.py
@@ -6,7 +6,6 @@ import datetime
 class Solver:
     def __init__(self, dates):
         self.dates = sorted(dates, key=lambda tup: (tup[1], tup[0]) )
-        print(self.dates)
 
     def format_result(self, result):
         return result.strftime('%m-%d')
@@ -17,19 +16,25 @@ class Solver:
         diff = self.get_days_diff(d1, m1, d2, m2)
         longest = diff
         dates_with_longest_diff = [(self.dates[len(self.dates) - 1], self.dates[0])]
-        print((self.dates[0], self.dates[len(self.dates) - 1]), diff)
         for i in range(1, len(self.dates)):
             d1, m1 = self.dates[i-1]
             d2, m2 = self.dates[i]
             diff = self.get_days_diff(d1, m1, d2, m2)
-            print((self.dates[i-1], self.dates[i]), diff)
             if diff > longest:
                 dates_with_longest_diff = [(self.dates[i-1], self.dates[i])]
+                longest = diff
             elif diff == longest:
                 dates_with_longest_diff.append((self.dates[i-1], self.dates[i]))
-        # TODO pick nearest 27th of October
-        d, m = dates_with_longest_diff[0][1]
-        return self.get_previous_date(d, m)
+        shortest_diff_to_27_oct = 356
+        ans_d = ans_m = None
+        for date in dates_with_longest_diff:
+            d, m = date[1]
+            diff =self.get_days_diff(27, 10, d, m)
+            if diff < shortest_diff_to_27_oct and date[1] != (28, 10):
+                shortest_diff_to_27_oct = diff
+                ans_d = d
+                ans_m = m
+        return self.get_previous_date(ans_d, ans_m)
 
     def get_days_diff(self, d1, m1, d2, m2):
         year1 = 2022
